77194012eff8af5ef030b55b39f4104194ee0e33
mgolestanfar@gmail.com <mgolestanfar@gmail.com@382b4e24-d1ed-dfe6-cee2-8b6670b2b11f>
2014-08-08 03:57:30 +0000

    test

--- trunk/PatentSearch/src/nicta/com/au/analysis/test.java
+++ trunk/PatentSearch/src/nicta/com/au/analysis/test.java
@@ -2,6 +2,6 @@ package nicta.com.au.analysis;
 
 public class test {
 	public static void main(String[] args) {
-		System.out.println("It's just to test my subversion!");
+		System.out.println("test my subversion!");
 	}
 }
