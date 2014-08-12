97b8b37233a4a42e8c88389265d3120aa30f08de
mgolestanfar <mgolestanfar@gmail.com>
2014-08-12 01:01:06 +0000

    added new classes

---
+++ PatentSearch/src/nicta/com/au/failureanalysis/evaluate/EvaluateResults.java
@@ -0,0 +1,133 @@
+package nicta.com.au.failureanalysis.evaluate;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class EvaluateResults {
+
+	public String _queryID = null;
+
+	public ArrayList<String> evaluatePatents(String queryID, String select/*
+																		 * boolean
+																		 * TP
+																		 */)
+			throws IOException {
+
+		ArrayList<String> TPs = new ArrayList<>();
+		ArrayList<String> FPs = new ArrayList<>();
+		ArrayList<String> FNs = new ArrayList<>();
+
+		_queryID = queryID.toUpperCase();
+
+		QueryAndPatents qps = new QueryAndPatents();
+
+		HashMap<String, ArrayList<String>> _reldocs = qps
+				.GetQueryPatents("data/qrel/PAC_test_rels.txt");
+
+		HashMap<String, ArrayList<String>> _retdocs = qps
+				.GetQueryPatents("output/results/results-lmdir-desc-100.txt");
+
+		ArrayList<String> relevantdocs = _reldocs.get(_queryID);
+		ArrayList<String> retrieveddocs = _retdocs.get(_queryID);
+
+		int i = 0;
+		// System.out.print("Retrieved Documents: ");
+
+		for (String x : retrieveddocs) {
+			i++;
+			// System.out.print(" [" + i + "] " + x);
+		}
+
+		// System.out.println();
+		int j = 0;
+		// System.out.print("Relevant Documents: ");
+		for (String x : relevantdocs) {
+			j++;
+			// System.out.print(" [" + j + "] " + x);
+		}
+
+		// System.out.println();
+
+		for (String ret : retrieveddocs) {
+			for (String rel : relevantdocs) {
+
+				if (ret.equals(rel)) {
+					// System.out.println(" matched: " + rel);
+					TPs.add(rel);
+					break;
+				}
+			}
+		}
+
+		for (String tp : TPs) {
+			retrieveddocs.remove(tp);
+			FPs = retrieveddocs;
+		}
+
+		for (String tp : TPs) {
+			relevantdocs.remove(tp);
+			FNs = relevantdocs;
+		}
+		
+
+		/*
+		 * for(String r : TPs){ System.out.println(r); }
+		 */
+
+		if (select.equals("TP")) {
+			return TPs;
+		} else {
+			if (select.equals("FP")) {
+				return FPs;
+			} else {
+				return FNs;
+			}
+		}
+
+		/*
+		 * if (TP) { return TPs; } else { return FPs; }
+		 */
+	}
+	
+public static void main(String[] args) throws IOException {
+		
+		EvaluateResults er = new EvaluateResults();
+//		er.evaluatePatents("PAC-1149", "TP");
+		/*ArrayList<String> tps = er.evaluatePatents("PAC-544", "TP");
+		ArrayList<String> fps = er.evaluatePatents("PAC-544", "FP");
+		ArrayList<String> fns = er.evaluatePatents("PAC-544", "FN");*/
+//		er.evaluatePatents("PAC-825", "TP");
+		ArrayList<String> tps = er.evaluatePatents("PAC-825", "TP");
+		ArrayList<String> fps = er.evaluatePatents("PAC-825", "FP");
+		ArrayList<String> fns = er.evaluatePatents("PAC-825", "FN");
+		int n = 0; 
+		  for (String tp : tps) { 
+			  n++; 
+			  			  			  
+			  System.out.println(" [" + n + "], [" + tp + "]"); 
+			  }	
+		  
+		  System.out.println("---------------------------------------------------- ");
+		  
+		  
+		  int m = 0; 
+		  for (String fp : fps) { 
+			  m++; 
+			  			  			  
+			  System.out.println(" [" + m + "], [" + fp + "]"); 
+			  }	
+
+		  System.out.println("---------------------------------------------------- ");
+		  
+		  int k = 0; 
+		  for (String fn : fns) { 
+			  k++; 
+			  			  			  
+			  System.out.println(" [" + k + "], [" + fn + "]"); 
+			  }		 
+
+
+		}
+
+}
---
+++ PatentSearch/src/nicta/com/au/failureanalysis/evaluate/QueryAndPatents.java
@@ -0,0 +1,153 @@
+package nicta.com.au.failureanalysis.evaluate;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map.Entry;
+
+/**
+ * @author mona
+ * 
+ */
+public class QueryAndPatents {
+
+	public String _filename = null;
+
+	/**
+	 * @param filename
+	 * @return query and related patents- retrieved patents in result file OR relevant patents in qrel file-
+	 * @throws IOException
+	 */
+	public HashMap<String, ArrayList<String>> GetQueryPatents(String filename)
+			throws IOException {
+
+		_filename = filename;
+
+		BufferedReader br = new BufferedReader(new FileReader(_filename));
+		String line;
+
+		HashMap<String, ArrayList<String>> query_patents = new HashMap<String, ArrayList<String>>();
+		ArrayList<String> patents = new ArrayList<String>();
+		// HashMap<String, String> doc_rank = new HashMap<String, String>();
+
+		String[] cols = null;
+		String current_quryID;
+		String previous_quryID = null;
+
+		int linenum = 0;
+
+		while ((line = br.readLine()) != null) {
+			linenum++;
+			cols = line.split(" ");
+			current_quryID = cols[0];
+
+			if (!current_quryID.equals(previous_quryID) && linenum != 1) {
+				// add a new entry for this column number
+				query_patents.put(previous_quryID, patents);
+				patents = new ArrayList<String>();
+			}
+			patents.add(cols[2]);
+			// doc_rank.put(cols[2], cols[3]);
+			previous_quryID = current_quryID;
+
+		}
+
+		query_patents.put(previous_quryID, patents);
+
+		br.close();
+
+		return query_patents;
+
+	}
+
+	/**
+	 * @param filename
+	 * @return
+	 * @throws IOException
+	 */
+	public HashMap<String, HashMap<String, String>> GetQueryPatentsRanks(
+			String filename) throws IOException {
+
+		_filename = filename;
+
+		BufferedReader br = new BufferedReader(new FileReader(_filename));
+		String line;
+
+		// HashMap<String, ArrayList<String>> query_docs = new HashMap<String,
+		// ArrayList<String>>();
+		HashMap<String, String> patents_ranks = new HashMap<String, String>();
+		HashMap<String, HashMap<String, String>> query_patent_rank = new HashMap<String, HashMap<String, String>>();
+
+		String[] cols = null;
+		String current_quryID;
+		String previous_quryID = null;
+
+		int linenum = 0;
+
+		while ((line = br.readLine()) != null) {
+			linenum++;
+			cols = line.split(" ");
+			current_quryID = cols[0];
+
+			if (!current_quryID.equals(previous_quryID) && linenum != 1) {
+				// add a new entry for this column number
+				query_patent_rank.put(previous_quryID, patents_ranks);
+				patents_ranks = new HashMap<String, String>();
+			}
+
+			patents_ranks.put(cols[2], cols[3]);
+			previous_quryID = current_quryID;
+
+		}
+
+		query_patent_rank.put(previous_quryID, patents_ranks);
+
+		br.close();
+
+		return query_patent_rank;
+
+	}
+
+	public static void main(String[] args) throws IOException {
+
+		/*-------------------------- Write in outputfile. --------------------------*/
+		String outputfile = "./output/test.txt";		
+		FileOutputStream out = new FileOutputStream(outputfile);
+		PrintStream ps = new PrintStream(out);
+		/*-------------------------------------------------------------------------*/
+
+		QueryAndPatents qps = new QueryAndPatents();
+		HashMap<String, ArrayList<String>> _querypatents = qps
+//						.GetQueryPatents("output/results/results-lmdir-desc-100.txt");
+		 .GetQueryPatents("data/qrel/PAC_test_rels.txt");
+		
+		int i = 0;
+		for (Entry<String, ArrayList<String>> k : _querypatents.entrySet()) {
+			i++;
+			ArrayList<String> patents = _querypatents.get(k.getKey());
+			// System.out.println(patents.size());
+//			System.out.println(" [" + i + "] " + k.getKey() + " , " + patents);
+//			ps.println(" [" + i + "] " + k.getKey() + " , " + patents);
+		}
+
+		HashMap<String, HashMap<String, String>> _docranks = qps
+				.GetQueryPatentsRanks("output/results/results-lmdir-desc-100.txt");
+		
+		
+
+		int j = 0;
+		for (Entry<String, HashMap<String, String>> dr : _docranks.entrySet()) {
+			j++;
+
+			System.out.println(" [" + j + "] " + dr.getKey() + " , "
+					+ _docranks.get(dr.getKey()));
+		}
+
+	}
+
+}
