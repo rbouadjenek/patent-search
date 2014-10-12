30db1449539cbd550e44fa874ebf2ca6e8476760
mgolestanfar <mgolestanfar@gmail.com>
2014-10-12 21:42:23 +0000

    overlap of good terms with query

--- PatentSearch/src/nicta/com/au/failureanalysis/goodterms/GoodTerms.java
+++ PatentSearch/src/nicta/com/au/failureanalysis/goodterms/GoodTerms.java
@@ -4,43 +4,36 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.TreeMap;
 import java.util.Map.Entry;
 
 import nicta.com.au.failureanalysis.evaluate.EvaluateResults;
 import nicta.com.au.failureanalysis.search.CollectionReader;
 import nicta.com.au.patent.document.PatentDocument;
 import nicta.com.au.patent.pac.evaluation.TopicsInMemory;
-import nicta.com.au.patent.pac.index.TermFreqVector;
 
-import org.apache.lucene.index.DocsEnum;
-import org.apache.lucene.index.Fields;
 import org.apache.lucene.index.IndexReader;
-import org.apache.lucene.index.MultiFields;
-import org.apache.lucene.index.Terms;
-import org.apache.lucene.index.TermsEnum;
 import org.apache.lucene.queryparser.classic.ParseException;
-import org.apache.lucene.search.DocIdSetIterator;
-import org.apache.lucene.util.BytesRef;
 
 public class GoodTerms {
 	static String querypath = "data/CLEF-IP-2010/PAC_test/topics/";	
-	static String indexDir = "data/INDEX/indexWithoutSW-Vec-CLEF-IP2010"/*"data/DocPLusQueryINDEX"*//*"data/QINDEX"*/;
+	static String indexDir = /*"data/INDEX/indexWithoutSW-Vec-CLEF-IP2010"*/"data/DocPLusQueryINDEX"/*"data/QINDEX"*/;
 
-	public void getGoodTerms() throws IOException, ParseException{
+	public TreeMap<String, Float> getTermsScoresPair() throws IOException, ParseException{
 		/*--------------------------- Write in output file. ------------------------*/
 		String outputfile = "./output/score/test.txt";
 
 		FileOutputStream out = new FileOutputStream(outputfile);
 		PrintStream ps = new PrintStream(out);
 		/*-------------------------------------------------------------------------------*/
-		
+				
+		TreeMap<String,Float> termsscoressorted = null;
 		CollectionReader reader = new CollectionReader(indexDir); 
-		IndexReader ir = reader.getIndexReader();
-		
-		
+		IndexReader ir = reader.getIndexReader();		
 					
 		TopicsInMemory topics = new TopicsInMemory("data/CLEF-IP-2010/PAC_test/topics/PAC_topics-test2.xml");
 		for(Map.Entry<String, PatentDocument> topic : topics.getTopics().entrySet()){
@@ -51,79 +44,187 @@ public class GoodTerms {
 			
 			System.out.println("=========================================");
 			System.out.println(queryName + "\t" +qUcid);
-			
+			System.out.println("=========================================");
 			/*int docid = reader.getDocId("UN-EP-0663270", PatentDocument.FileName);
 			ir.getTermVector(docid, field) getTermVectors(b);*/
 			
 			EvaluateResults er = new EvaluateResults();
 			ArrayList<String> tps = er.evaluatePatents(queryid, "TP");
 			ArrayList<String> fps = er.evaluatePatents(queryid, "FP");
-			HashSet<String> vocabularies = new HashSet<String>();
-			HashMap<String, Integer> TFs = new HashMap<>();
+			HashMap<String, Float> /*TFreqs*/ termsscores = new HashMap<>();
 			
+			
+/*--------------------------------- Query Words -------------------------------*/
+//			HashMap<String, Integer> query_termsfreqspair = reader.gettermfreqpair(qUcid, PatentDocument.Description);
+			HashSet<String> query_terms = reader.getDocTerms(qUcid, PatentDocument.Description);
+//			System.out.println(query_termsfreqspair.size() +"\t"+ query_termsfreqspair);
+			System.out.println(query_terms.size() + "\t" + query_terms);
+/*-----------------------------------------------------------------------------*/			
+			
+			
+//			System.out.println("-----TPs----");
 			for (String tp : tps) {
-//				System.out.println(tp);
-				HashMap<String, Integer> termsfreqs = reader.gettermfreqpair("UN-" + tp, PatentDocument.Title);
-				for(Entry<String, Integer> t:termsfreqs.entrySet()){
-					if(TFs.containsKey(t.getKey())){
-						TFs.put(t.getKey(), TFs.get(t.getKey())+t.getValue()/tps.size());
+				/*System.out.println("---------");
+				System.out.println(tp);*/
+				HashMap<String, Integer> termsfreqsTP = reader.gettermfreqpairAllsecs("UN-" + tp);
+				
+				for(Entry<String, Integer> tfTP:termsfreqsTP.entrySet()){
+					if(termsscores.containsKey(tfTP.getKey())){
+						termsscores.put(tfTP.getKey(), termsscores.get(tfTP.getKey()) + (float)tfTP.getValue()/tps.size());
 					}else{
-						
+//						float test = (float)t.getValue()/tps.size();
+//						System.out.println(test);
+						termsscores.put(tfTP.getKey(), (float)tfTP.getValue()/tps.size());
 					}
 				}
-
-				
+//				System.out.println(termsscores.size() + " " + termsscores);					
 			}
 			
+			/*System.out.println();
+			System.out.println("-----FNs----");*/
 			for (String fp : fps) {
+				/*System.out.println("---------");
+				System.out.println(fp);*/
+				HashMap<String, Integer> termsfreqsFP = reader.gettermfreqpairAllsecs("UN-" + fp);
 				
+				for(Entry<String, Integer> t:termsfreqsFP.entrySet()){
+					if(termsscores.containsKey(t.getKey())){
+						termsscores.put(t.getKey(), termsscores.get(t.getKey()) - (float)t.getValue()/fps.size());
+					}else{						
+						termsscores.put(t.getKey(), -(float)t.getValue()/fps.size());
+					}
+				}
+//				System.out.println(TFreqs.size() + " " + TFreqs);
 			}
-			
-			
-			
-//			System.out.println(tps);
-			for (String doc : tps) {
-				vocabularies.addAll(reader.getDocTerms("UN-" + doc, PatentDocument.Title));
-				
-				/*System.out.println("-----------------------");
-				System.out.println("new doc : " + vocabularies.size());*/
-				if(vocabularies!=null){
-					if(reader.getDocTerms("UN-" + doc, PatentDocument.Description)!=null){vocabularies.addAll(reader.getDocTerms("UN-" + doc, PatentDocument.Description));}
-					if(reader.getDocTerms("UN-" + doc, PatentDocument.Abstract)!=null){vocabularies.addAll(reader.getDocTerms("UN-" + doc, PatentDocument.Abstract));}
-					if(reader.getDocTerms("UN-" + doc, PatentDocument.Claims)!=null){vocabularies.addAll(reader.getDocTerms("UN-" + doc, PatentDocument.Claims));}
+//			System.out.println(termsscores.size() + " " + termsscores);
+			ValueComparator bvc =  new ValueComparator(termsscores);
+			/*TreeMap<String,Float> */termsscoressorted = new TreeMap<String,Float>(bvc);
+			termsscoressorted.putAll(termsscores);
+			int overlap = 0;
+			int i = 0;
+			for(Entry<String, Float> scoresorted:termsscoressorted.entrySet()){
+				i++;
+				if(i<=100){
+					if(query_terms.contains(scoresorted.getKey())){
+						overlap++;
+					}
+					System.out.println("["+ i +"]" + scoresorted.getKey()+"\t"+scoresorted.getValue());
 				}
 			}
-//			System.out.println(vocabularies.size());
-//			System.out.println(vocabularies);
-			for (String doc : fps) {
-				if(vocabularies!=null){
-					if(reader.getDocTerms("UN-" + doc, PatentDocument.Title)!=null){vocabularies.addAll(reader.getDocTerms("UN-" + doc, PatentDocument.Title));}
-					if(reader.getDocTerms("UN-" + doc, PatentDocument.Description)!=null){vocabularies.addAll(reader.getDocTerms("UN-" + doc, PatentDocument.Description));}
-					if(reader.getDocTerms("UN-" + doc, PatentDocument.Abstract)!=null){vocabularies.addAll(reader.getDocTerms("UN-" + doc, PatentDocument.Abstract));}
-					if(reader.getDocTerms("UN-" + doc, PatentDocument.Claims)!=null){vocabularies.addAll(reader.getDocTerms("UN-" + doc, PatentDocument.Claims));}
-				}
-			}
-			System.out.println(vocabularies.size());
-			System.out.println(vocabularies);
-//			for(String v:vocabularies){
-				/*for (String tp : tps) {
-					System.out.println(tp);
-//					vocabularies.contains(o);
-					
-				}
-				
-				for (String fp : fps) {
-					
-				}*/
+			
+//			System.out.println(termsscoressorted.size() + " " + termsscoressorted);
+//			int i = 0;
+//			for(Entry<String, Float> scoresorted:termsscoressorted.entrySet()){
+//				i++;
+////				if(i<=100){
+//					System.out.println("["+ i +"]" + scoresorted.getKey()+"\t"+scoresorted.getValue());
+////				}
 //			}
+			
+			
+			
+
+			
+		}
+		return termsscoressorted;			
+	}
+	
+	public HashMap<String, Float> getUsefulTermsScores(String status) throws IOException, ParseException{
+		GoodTerms gt = new GoodTerms();
+		TreeMap<String, Float> termsscores_sorted = gt.getTermsScoresPair();
+		HashMap<String, Float> useful_terms = new HashMap<>();
+		HashMap<String, Float> non_useful_terms = new HashMap<>();
+		for(Entry<String, Float> scoresorted:termsscores_sorted.entrySet()){
+			if(scoresorted.getValue()>0){				
+				useful_terms.put( scoresorted.getKey(), scoresorted.getValue());
+			}else{
+				non_useful_terms.put( scoresorted.getKey(), scoresorted.getValue());
+			}
+		}
+		if(status.equals("useful")){
+			return useful_terms;	
+		}
+		if(status.equals("non-useful")){
+			return non_useful_terms;	
+		}
+			return useful_terms;			
+	}
+	
+	public TreeMap<String, Float> getTopnTermsScores(int n) throws IOException, ParseException{		
+		GoodTerms gt = new GoodTerms();
+		TreeMap<String, Float> termsscores_sorted = gt.getTermsScoresPair();
+		TreeMap<String, Float> topn_terms = new TreeMap<String, Float>();
+		int i = 0;
+		for(Entry<String, Float> scoresorted:termsscores_sorted.entrySet()){
+			i++;
+			if(i<=n){
+				System.out.println("["+ i +"] " + scoresorted.getKey()+"\t"+scoresorted.getValue());
+				topn_terms.put(scoresorted.getKey(), scoresorted.getValue());
+			}else {
+				break;
+			}
 		}
 		
-		
+		return topn_terms;		
 	}
 	
 	public static void main(String[] args) throws IOException, ParseException {
 		GoodTerms gt = new GoodTerms();
-		gt.getGoodTerms();
+		
+		/*------------------ Test: get all terms_score pairs from qrel file --------------------*/
+		TreeMap<String, Float> termsscores_sorted = gt.getTermsScoresPair();
+		System.out.println(termsscores_sorted.size() + " " + termsscores_sorted);
+		int i = 0;
+		for(Entry<String, Float> scoresorted:termsscores_sorted.entrySet()){
+			i++;
+//			if(i<=100){
+				System.out.println("["+ i +"]" + scoresorted.getKey()+"\t"+scoresorted.getValue());
+//			}
+		}
+		/*--------------------------------------------------------------------------------------------*/
+		
+		/*---------------------- Test: get useful terms_score pairs from qrel file (score>0) ----------------------*/
+//		HashMap<String, Float> useful_termsscores_sorted = gt.getUsefulTermsScores("useful");
+//		HashMap<String, Float> non_useful_termsscores_sorted = gt.getUsefulTermsScores("non-useful");
+//		int j =0;
+//		for(Entry<String, Float> scoresorted:useful_termsscores_sorted.entrySet()){
+//			j++;
+//			System.out.println("["+ j +"]" + scoresorted.getKey()+"\t"+scoresorted.getValue());
+//		}
+//		System.out.println("========================================");
+//		int k =0;
+//		for(Entry<String, Float> scoresorted:non_useful_termsscores_sorted.entrySet()){
+//			k++;
+//			System.out.println("["+ k +"]" + scoresorted.getKey()+"\t"+scoresorted.getValue());
+//		}
+//		
+		/*--------------------------------------------------------------------------------------------*/
+		
+		/*---------------------- Test: get top-n(100) terms_score pairs from qrel file (score>0) ----------------------*/
+//		gt.getTopnTermsScores(100);
+		/*--------------------------------------------------------------------------------------------*/
+		
 		
 	}
 }
+
+
+/*-------------- This class is used to sort a hashmap --------------*/
+
+class ValueComparator implements Comparator<String> {
+
+	Map<String, Float> base;
+	public ValueComparator(Map<String, Float> base) {
+		this.base = base;
+	}
+
+	// Note: this comparator imposes orderings that are inconsistent with equals.    
+	public int compare(String a, String b) {
+		if (base.get(a) >= base.get(b)) {
+			return -1;
+		} else {
+			return 1;
+		} // returning 0 would merge keys
+	}
+}
+
