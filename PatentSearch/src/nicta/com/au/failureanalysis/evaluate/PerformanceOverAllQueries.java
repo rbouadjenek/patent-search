ffb51453844abc5e5c4d9540f79f8ffaa8b45007
mgolestanfar <mgolestanfar@gmail.com>
2014-10-17 08:01:43 +0000

    calculate avg precision

--- PatentSearch/src/nicta/com/au/failureanalysis/evaluate/PerformanceOverAllQueries.java
+++ PatentSearch/src/nicta/com/au/failureanalysis/evaluate/PerformanceOverAllQueries.java
@@ -5,27 +5,35 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.TreeMap;
 
 import nicta.com.au.patent.document.PatentDocument;
 import nicta.com.au.patent.pac.evaluation.TopicsInMemory;
 
 public class PerformanceOverAllQueries {
 
+	EvaluateResults er = new EvaluateResults();
+	AnalyseFNs afn = new AnalyseFNs();
+	QueryAndPatents qps = new QueryAndPatents();
+	
 	public void calculateRecall() throws IOException {
 		/*--------------------------- Write in output file. ------------------------*/
-		String outputfile = "./output/Recall/recallfinal.txt";
+		String outputfile = "./output/Performance/recallfinal.txt";
 
 		FileOutputStream out = new FileOutputStream(outputfile);
 		PrintStream ps = new PrintStream(out);
 		/*-------------------------------------------------------------------------------*/
-		EvaluateResults er = new EvaluateResults();
-		AnalyseFNs afn = new AnalyseFNs();
+//		EvaluateResults er = new EvaluateResults();
+//		AnalyseFNs afn = new AnalyseFNs();
 		
 		System.out.println("Query id"+"\t" + "Recall" + "\t" + "EnFn_Recall");
 		ps.println("Query id"+"\t" + "Recall" + "\t" + "EnFn_Recall");
 		System.out.println("=====================================");
-		TopicsInMemory topics = new TopicsInMemory("data/CLEF-IP-2010/PAC_test/topics/PAC_topics-omit-PAC-1094.xml");
+		TopicsInMemory topics = new TopicsInMemory("data/CLEF-IP-2010/PAC_test/topics/PAC_topics-test2.xml");
 		for(Map.Entry<String, PatentDocument> topic : topics.getTopics().entrySet()){
 			String qUcid = topic.getValue().getUcid();
 			String queryid = topic.getKey();
@@ -54,9 +62,93 @@ public class PerformanceOverAllQueries {
 		}
 	}
 	
+	public void calculateAvgPrecision() throws IOException {
+		
+		/*--------------------------- Write in output file. ------------------------*/
+		String outputfile = "./output/Performance/test.txt";
+
+		FileOutputStream out = new FileOutputStream(outputfile);
+		PrintStream ps = new PrintStream(out);
+		/*-------------------------------------------------------------------------------*/
+		
+		TreeMap<String,Integer> TPs_ranks_sorted = null;
+		
+		System.out.println("QueryId"+"\t" + "EnFn_Avg.P" + "\t" + "Avg. P");
+		ps.println("QueryId"+"\t" + "EnFn_Avg.P" + "\t" + "Avg. P");
+		System.out.println("=====================================");
+		TopicsInMemory topics = new TopicsInMemory("data/CLEF-IP-2010/PAC_test/topics/PAC_topics.xml");
+		for(Map.Entry<String, PatentDocument> topic : topics.getTopics().entrySet()){
+			String qUcid = topic.getValue().getUcid();
+			String queryid = topic.getKey();
+			String queryName = topic.getKey() + "_" + topic.getValue().getUcid();
+			String queryfile = topic.getKey() + "_" + topic.getValue().getUcid() + ".xml";			
+			
+			ArrayList<String> tps = er.evaluatePatents(queryid, "TP");
+			ArrayList<String> fns = er.evaluatePatents(queryid, "FN");
+			ArrayList<String> enfns = afn.getEnglishFNs(queryid);
+			
+			int enfnsize = enfns.size();			
+		    int tpsize = tps.size();
+			int fnsize = fns.size();
+			int qrelsize = tpsize + fnsize;
+			int excludedqrelsize = tpsize + enfnsize;
+			
+			HashMap<String, HashMap<String, String>> _docranks = qps.GetQueryPatentsRanks("output/results/results-lmdir-desc-100.txt");
+			/*System.out.println("--------");
+			System.out.println(queryid);
+			System.out.println("--------");*/
+			Map<String, Integer> TPs_ranks = new HashMap<String, Integer>();
+//			Map<String, String> treeMap = new TreeMap<String, String>();
+			for(String tp:tps){
+				TPs_ranks.put(tp, Integer.parseInt(_docranks.get(queryid).get(tp)));
+				
+//				System.out.println(tp+"\t"+_docranks.get(queryid).get(tp));
+			}
+			ValueComparator bvc =  new ValueComparator(TPs_ranks);
+			TPs_ranks_sorted = new TreeMap<String,Integer>(bvc);
+			TPs_ranks_sorted.putAll(TPs_ranks);
+//			System.out.println(TPs_ranks_sorted);
+			
+			int j = 0;
+			float sum = 0;
+			for( Entry<String, Integer> tp_rank : TPs_ranks_sorted.entrySet()){
+				j++;
+				Integer rank = tp_rank.getValue();
+				float prec_at = (float)j/rank;
+//				System.out.println(rank +"\t"+prec_at);
+				sum = sum + prec_at;
+			}
+			float avg_precision_excluded = (float)sum/excludedqrelsize;
+			float avg_precision = (float)sum/qrelsize; 
+			System.out.println(queryid + "\t" + avg_precision_excluded + "\t" + avg_precision); 
+			ps.println(queryid + "\t" + avg_precision_excluded + "\t" + avg_precision); 
+		}
+	}
+	
 	
 	public static void main(String[] args) throws IOException {
 		PerformanceOverAllQueries performance = new PerformanceOverAllQueries();
-		performance.calculateRecall();
+//		performance.calculateRecall();
+		performance.calculateAvgPrecision();
+	}
+}
+
+/*-------------- This class is used to sort a hashmap --------------*/
+
+class ValueComparator implements Comparator<String> {
+
+	Map<String, Integer> base;
+	public ValueComparator(Map<String, Integer> base) {
+		this.base = base;
+	}
+
+	// Note: this comparator imposes orderings that are inconsistent with equals.    
+	public int compare(String a, String b) {
+//		if (base.get(a) >= base.get(b)) {
+		if (base.get(b) >= base.get(a)) {
+			return -1;
+		} else {
+			return 1;
+		} // returning 0 would merge keys
 	}
 }
--- PatentSearch/src/nicta/com/au/failureanalysis/goodterms/PositiveTermsOverlapTPs.java
+++ PatentSearch/src/nicta/com/au/failureanalysis/goodterms/PositiveTermsOverlapTPs.java
@@ -22,7 +22,7 @@ import org.apache.lucene.queryparser.classic.ParseException;
 
 public class PositiveTermsOverlapTPs {
 	static String indexDir = /*"data/INDEX/indexWithoutSW-Vec-CLEF-IP2010"*/"data/DocPLusQueryINDEX"/*"data/QINDEX"*/;
-	
+
 	public static void main(String[] args) throws IOException, ParseException {
 		/*--------------------------- Write in output file. ------------------------*/
 		String outputfile = "./output/score/termoverlp-TPs2.txt";
@@ -46,71 +46,71 @@ public class PositiveTermsOverlapTPs {
 
 			int tpssize = tps.size();
 			if(tpssize != 0){
-			/*System.out.println("=========================================");
+				/*System.out.println("=========================================");
 			System.out.println(queryName + "\t" +qUcid);
 			System.out.println("=========================================");*/
 
-			Map<String, Float> tspairs = olap.getTermsScoresPair(queryid, qUcid);
-			HashMap<String, Float> tshashes = new HashMap<>();
-			tshashes.putAll(tspairs);
-			
-//			System.out.println(tspairs.size() + "\t" + tspairs);
-//			System.out.println(tshashes.size() + "\t" + tshashes.get("piston")+ "\t" + tshashes);
+				Map<String, Float> tspairs = olap.getTermsScoresPair(queryid, qUcid);
+				HashMap<String, Float> tshashes = new HashMap<>();
+				tshashes.putAll(tspairs);
 
-			/*ArrayList<String> enfns = afn.getEnglishFNs(queryid);
+				//			System.out.println(tspairs.size() + "\t" + tspairs);
+				//			System.out.println(tshashes.size() + "\t" + tshashes.get("piston")+ "\t" + tshashes);
+
+				/*ArrayList<String> enfns = afn.getEnglishFNs(queryid);
 			int n_enfns = enfns.size();*/
 
-			/*--------------------------------- Query Words -------------------------------*/			
-			//			HashMap<String, Integer> query_termsfreqspair = reader.gettermfreqpair(qUcid, PatentDocument.Description);
-			HashSet<String> query_terms = reader.getDocTerms(qUcid, PatentDocument.Description);
-			int querysize = query_terms.size();
-			//			System.out.println(query_termsfreqspair.size() +"\t"+ query_termsfreqspair);
-			//			System.out.println(query_terms.size() + "\t" + query_terms);
+				/*--------------------------------- Query Words -------------------------------*/			
+				//			HashMap<String, Integer> query_termsfreqspair = reader.gettermfreqpair(qUcid, PatentDocument.Description);
+				HashSet<String> query_terms = reader.getDocTerms(qUcid, PatentDocument.Description);
+				int querysize = query_terms.size();
+				//			System.out.println(query_termsfreqspair.size() +"\t"+ query_termsfreqspair);
+				//			System.out.println(query_terms.size() + "\t" + query_terms);
 
-			
-			int usefuloverlap;
-			int nonusefuloverlap;
-			int overlap;
-			float usefulsum=0;
-			float nonusefulsum=0;
-			float sum=0;
-			float usefulavg=0;
-			float nonusefulavg=0;
-			float avg=0;
-			for (String doc : tps) { 
-				usefuloverlap = 0;
-				nonusefuloverlap = 0;
-				overlap = 0;
-				Set<String> docterms = reader.gettermfreqpairAllsecs("UN-" + doc).keySet();
-				
-				for( String qt : query_terms){
 
-					boolean existsuseful = (docterms.contains(qt) && tshashes.get(qt)>0);						
-					if(existsuseful){
-						usefuloverlap++;	
-					}					
-					boolean existsnonuseful = (docterms.contains(qt) && tshashes.get(qt)<=0);						
-					if(existsnonuseful){
-						nonusefuloverlap++;	
+				int usefuloverlap;
+				int nonusefuloverlap;
+				int overlap;
+				float usefulsum=0;
+				float nonusefulsum=0;
+				float sum=0;
+				float usefulavg=0;
+				float nonusefulavg=0;
+				float avg=0;
+				for (String doc : tps) { 
+					usefuloverlap = 0;
+					nonusefuloverlap = 0;
+					overlap = 0;
+					Set<String> docterms = reader.gettermfreqpairAllsecs("UN-" + doc).keySet();
+
+					for( String qt : query_terms){
+
+						boolean existsuseful = (docterms.contains(qt) && tshashes.get(qt)>0);						
+						if(existsuseful){
+							usefuloverlap++;	
+						}					
+						boolean existsnonuseful = (docterms.contains(qt) && tshashes.get(qt)<=0);						
+						if(existsnonuseful){
+							nonusefuloverlap++;	
+						}
+						boolean exists = (docterms.contains(qt));						
+						if(exists){
+							overlap++;	
+						}
 					}
-					boolean exists = (docterms.contains(qt));						
-					if(exists){
-						overlap++;	
-					}
+					//				System.out.println((float)usefuloverlap/querysize+"\t"+(float)nonusefuloverlap/querysize+"\t"+(float)overlap/querysize);
+					usefulsum = usefulsum + (float)usefuloverlap/querysize;
+					nonusefulsum = nonusefulsum + (float)nonusefuloverlap/querysize;
+					sum = sum + (float)overlap/querysize;
+
 				}
-//				System.out.println((float)usefuloverlap/querysize+"\t"+(float)nonusefuloverlap/querysize+"\t"+(float)overlap/querysize);
-				usefulsum = usefulsum + (float)usefuloverlap/querysize;
-				nonusefulsum = nonusefulsum + (float)nonusefuloverlap/querysize;
-				sum = sum + (float)overlap/querysize;
-				
+
+				usefulavg = (float)usefulsum/tpssize;
+				nonusefulavg = (float)nonusefulsum/tpssize;
+				avg = (float)sum/tpssize;
+				System.out.println(queryName+"\t"+usefulavg+"\t"+nonusefulavg+"\t"+avg);
+				ps.println(queryName+"\t"+usefulavg+"\t"+nonusefulavg+"\t"+avg);
 			}
-			
-			usefulavg = (float)usefulsum/tpssize;
-			nonusefulavg = (float)nonusefulsum/tpssize;
-			avg = (float)sum/tpssize;
-			System.out.println(queryName+"\t"+usefulavg+"\t"+nonusefulavg+"\t"+avg);
-			ps.println(queryName+"\t"+usefulavg+"\t"+nonusefulavg+"\t"+avg);
-		}
 		}/*else{
 			System.out.println(queryName+"\t" + "No TP for this query");
 			ps.println(queryName+"\t"+ "No TP for this query");
