66d484b55dbc387615e4bfc6cba26985920462d9
mgolestanfar <mgolestanfar@382b4e24-d1ed-dfe6-cee2-8b6670b2b11f>
2014-12-24 01:11:12 +0000

    test sub

--- PatentSearch/src/nicta/com/au/analysis/test.java
+++ PatentSearch/src/nicta/com/au/analysis/test.java
@@ -3,5 +3,6 @@ package nicta.com.au.analysis;
 public class test {
 	public static void main(String[] args) {
 		System.out.println("test my subversion from home!");
+		System.out.println("test");
 	}
 }
