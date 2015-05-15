c32a5631d4f3f8bdcda5e6bd2d0a8ac2fbba84c2
mgolestanfar@gmail.com <mgolestanfar@gmail.com@382b4e24-d1ed-dfe6-cee2-8b6670b2b11f>
2015-05-15 05:43:31 +0000

    thesis

--- trunk/SIGIR2015-ShortPaper/SIGIR2015.sh
+++ trunk/SIGIR2015-ShortPaper/SIGIR2015.sh
@@ -1,11 +1,26 @@
-latex main.tex 
-bibtex main.aux
-dvips -t letter main.dvi
-latex main.tex 
-bibtex main.aux
-dvips -t letter main.dvi
-latex main.tex 
-bibtex main.aux
-dvips -t letter main.dvi
-ps2pdf -dMaxSubsetPct=100 -dSubsetFonts=true -dEmbedAllFonts=true -dPDFSETTINGS=/printer -dUseCIEColor main.ps
+#latex main.tex 
+#bibtex main.aux
+#dvips -t letter main.dvi
+#latex main.tex 
+#bibtex main.aux
+#dvips -t letter main.dvi
+#latex main.tex 
+#bibtex main.aux
+#dvips -t letter main.dvi
+#ps2pdf -dMaxSubsetPct=100 -dSubsetFonts=true -dEmbedAllFonts=true -dPDFSETTINGS=/printer -dUseCIEColor main.ps
 #pdffonts main.pdf
+-----------------
+latex CameraReady.tex 
+bibtex CameraReady.aux
+dvips -t letter CameraReady.dvi
+latex CameraReady.tex 
+bibtex CameraReady.aux
+dvips -t letter CameraReady.dvi
+latex CameraReady.tex 
+bibtex CameraReady.aux
+dvips -t letter CameraReady.dvi
+#ps2pdf -dPDFSETTINGS=/prepress -dSubsetFonts=true -dEmbedAllFonts=true -dMaxSubsetPct=100 -dCompatibilityLevel=1.3 CameraReady.ps
+ ps2pdf -dMaxSubsetPct=100 -dSubsetFonts=true -dEmbedAllFonts=true -dPDFSETTINGS=/printer -dUseCIEColor CameraReady.ps
+#pdffonts CameraReady.pdf
+#pdftops CameraReady.pdf
+#ps2pdf14 -dPDFSETTINGS=/prepress CameraReady.ps
